a9d8bfce79b85f7706f18a7524cff18bf4c02e33
Oliver <onbjerg@users.noreply.github.com>
2018-12-11 09:28:24 +0100

    Clean up scene hit detection

--- src/scenery/scene.rs
+++ src/scenery/scene.rs
@@ -23,20 +23,7 @@ impl Hitable for Scene {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<Hit> {
         self.spheres.iter().fold(
             None,
-            |hit, s| {
-                // TODO: Somehow clean up so u dont suck
-                match hit {
-                    Some(hit) => if let Some(h) = s.hit(ray, t_min, hit.t) {
-                        Some(h)
-                    } else {
-                        Some(hit)
-                    },
-                    None => if let Some(h) = s.hit(ray, t_min, t_max) {
-                        Some(h)
-                    } else {
-                        None
-                    }
-                }
+            |hit, s| s.hit(ray, t_min, hit.t).or(hit)
         })
     }
 }
